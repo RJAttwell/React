9a381126c2e48ab4fc9f76eb4aacf431c5763562
RJAttwell <richardjattwell@gmail.com>
2023-05-16 02:34:32 +0100

    Add files via upload

---
+++ Keeper App Challenge/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./components/Header";
+import Notes from "./components/Notes";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+
+      <Notes />
+      
+      <Footer />
+    
+    </div>
+
+  );
+}
+
+export default App;
