f3d6f99663d65b6681e9a82639c74219c3bbfff3
RJAttwell <richardjattwell@gmail.com>
2023-05-17 01:43:28 +0100

    Create contact.js

---
+++ React Props/Challenge/contact.js
@@ -0,0 +1,26 @@
+const contacts = [
+  {
+    name: "Chris Pratt",
+    imgURL: "https://flxt.tmsimg.com/assets/249381_v9_bb.jpg",
+    tel: "+12 345 6789",
+    email: "PrattPrattPratt@gmail.com"
+  },
+
+  {
+    name: "Jack Black",
+    imgURL:
+      "https://file1.closermag.fr/var/closermag/storage/images/bio-people/biographie-jack-black-112096/811354-1-fre-FR/Jack-Black.jpg?alias=exact1024x768_l",
+    tel: "+98 765 4321",
+    email: "Jables@gmail.com"
+  },
+
+  {
+    name: "Tom Brady",
+    imgURL:
+      "https://i2-prod.mirror.co.uk/incoming/article29200088.ece/ALTERNATES/s1200c/2_GettyImages-1461275923.jpg",
+    tel: "+28 312 7109",
+    email: "TruePatriot@gmail.com"
+  }
+];
+
+export default contacts;
