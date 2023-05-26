275026ff197a1a4f674a38bb5ab825bc7f72203c
RJAttwell <richardjattwell@gmail.com>
2023-05-26 12:53:04 +0100

    Update App.js

--- React Hooks/Clock Web App/App.js
+++ React Hooks/Clock Web App/App.js
@@ -15,6 +15,7 @@ import React, { useState, useEffect } from "react";
 export default function App() {
   //useState hook defines the time variable and the setTime function to update it
   const [time, setTime] = useState(new Date());
+  const [tokyoTime, setTokyoTime] = useState("");
 
   //useEffect hook which begins the interval timer and updates the time state every second.
   //useEffect allows you to perform side effects in function components.
@@ -24,8 +25,18 @@ export default function App() {
       setTime(new Date());
     }, 1000);
 
+    const tokyoTimer = setInterval(() => {
+      const tokyo = new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Tokyo"
+      });
+      setTokyoTime(new Date(tokyo));
+    }, 1000);
+
     //clearInterval cancels the interval timer.
-    return () => clearInterval(timer);
+    return () => {
+      clearInterval(timer);
+      clearInterval(tokyoTimer);
+    };
   }, []);
 
   //By putting an empty array, we are telling React to only run the effect once when the component is inititally
@@ -36,8 +47,12 @@ export default function App() {
       {/* Displays what timezone the current time is */}
       <h3>UTC +0 - United Kingdom, Ireland</h3>
       <h1>{time.toLocaleTimeString()}</h1>
+
+      <h3>UTC +9 - Japan, South Korea</h3>
+      <h1>{tokyoTime && tokyoTime.toLocaleTimeString()}</h1>
       {/* Button updates the time when it's clicked */}
       <button onClick={() => setTime(new Date())}>Get Time</button>
     </div>
   );
 }
+
